8c1fcacb83f47b3f39475d020e0fe20b3ed557bc
Jeet <jeet.lal1@tothenew.com>
2017-02-11 22:57:55 +0530

    manage organization

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,29 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
+  'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+  'bootstrap' => [
+        'languageSwitcher',
     ],
+  'components' => [
+    'cache' => [
+      'class' => '',
+    ],
+    'languageSwitcher' => [
+      'class' => 'common\components\languageSwitcher',
+    ],
+    'i18n' => [
+      'translations' => [
+        'app*' => [
+          'class' => 'yii\i18n\PhpMessageSource',
+          'basePath' => '@common/language',
+          'sourceLanguage' => ['en','fr'],
+          'fileMap' => [
+            'app' => 'app.php',
+            'app/error' => 'error.php',
+          ],
+          //'on missingTranslation' => ['common\components\TranslationEventHandler', 'handleMissingTranslation']
+        ],
+      ],
+    ],
+  ],
 ];
