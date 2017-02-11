319c2ac64b4bcd14cd7d424df9aabde7d77c13b4
Jeet <jeet.lal1@tothenew.com>
2017-02-11 22:58:28 +0530

    implement english translator

---
+++ common/language/fr/app.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'Dashboard' => 'France Dashboard',
+    'About' => 'Acerca de',
+    'Contact' => 'Contacto',
+    'Home' => 'Home',
+    'Logout' => 'Salir',
+    'My Company' => 'Mi Empresa',
+    'Sign In' => 'Entrar',
+    'Sign Up' => 'Registrarse',
+    'Status' => 'Estado',
+  ];
